55d558ab80af27c0191095339ec603d553af9892
jquie <jacquelynphan03@gmail.com>
2024-12-11 18:22:42 +0000

    not done

--- Main.java
+++ Main.java
@@ -4,11 +4,44 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Scanner sc = new Scanner(System.in);
     // TODO: Problem 1
-
+    double num1 = sc.nextDouble();
+    double num2 = sc.nextDouble();
+    if (num2 == 0)
+    {
+      System.out.println("Error");
+    }
+    else
+    {
+      if (1 < (num1 / num2) && (num1 / num2) <= 8)
+      {
+        System.out.println("Ratio OK");
+      }
+      else
+      {
+        System.out.println("Ratio NOT OK");
+      }
+    } 
 
     // TODO: Problem 2
-
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if (b == 0)
+    {
+      System.out.println("Error");
+    }
+    else
+    {
+      if (a % b == 0)
+      {
+        System.out.println("Is a factor");
+      }
+      else
+      {
+        System.out.println("Not a factor");
+      }
+    }
 
     // TODO: Problem 3
   }
